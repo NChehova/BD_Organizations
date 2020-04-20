74cf87f3183bf6c29556fb12bb2f34276911d6a4
ChehovaN <chhv.clb@gmail.com>
2020-04-20 21:09:44 +0300

    first_commit

---
+++ src/test/java/com/example/chhv/clb/OrganizationsBD/OrganizationsBdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chhv.clb.OrganizationsBD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrganizationsBdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
